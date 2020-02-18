0698ab10bb42e0687befce4f90c4aa8bf9056dd9
Anusri27 <Akhil9895>
2020-02-18 16:35:02 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/result.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 void main(){
   runApp(MyApp());
 }
@@ -13,26 +14,37 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyAppState extends State<MyApp>{
-   final question = const [
+   final _question = const [
       {'questiontext':'What is your fav place?',
-      'answers':['home','school','hostel','dreamland']
+      'answers':[{'text':'home','score':10},{'text':'school','score':5},{'text':'hostel','score':8},{'text':'dreamland','score':11}]
       },
       {'questiontext':'What is your fav clolor?',
-      'answers':['White','Blue','Black','Red']
+      'answers':[{'text':'White','score':10},{'text':'Blue','score':5},{'text':'Black','score':15},{'text':'Red','score':1}]
       },
       {'questiontext':'What is your fav animal?',
-      'answers':['rabbit','cat','dog','panda']
+      'answers':[{'text':'rabbit','score':3},{'text':'cat','score':2},{'text':'dog','score':100},{'text':'panda','score':20}]
       }
     ];
    var _qi=0;
+   var  _total=0;
+
+  void _reset(){
+    setState(() {
+       _qi=0;
+       _total=0;
+    });
+   
+  }
   
-  void _answerq(){
+  void _answerq(int score){
+    _total += score;
+    
     setState(() {
       _qi=_qi+1;
     });
     print(_qi);  
     
-    if(_qi<question.length)
+    if(_qi<_question.length)
     {
       print("You hve more left");
     }
@@ -46,16 +58,9 @@ class _MyAppState extends State<MyApp>{
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text("Quiz App") ,
       ) ,
-      body:(_qi < question.length) ? Column(children: [
-        Question(
-          question[_qi]['questiontext']
-        ),
-        ...(question[_qi]['answers'] as List<String>).map((answer){
-          return Answer(_answerq,answer);
-        }).toList()
-      ]
-        
-      ): Center(child: Text("You did it!!"),),
+      body:(_qi < _question.length) ? 
+      Quiz(answerq: _answerq,question: _question,qi: _qi):
+      Result(_total,_reset)
       ),
       );
   }
